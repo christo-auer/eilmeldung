7c09c593869d943976ebf6c9e65812703e37373c
Christopher Auer <christopher@auer.team>
2025-12-16 20:22:50 +0100

    added homebrew formular for release binaries

--- HomebrewFormula/eilmeldung.rb
+++ HomebrewFormula/eilmeldung.rb
@@ -7,16 +7,13 @@ class Eilmeldung < Formula
   
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/christo-auer/eilmeldung/releases/download/0.1.0/eilmeldung-aarch64-apple-darwin-0.1.0.tar.gz"
-
-    sha256 "8a1e54883035c97a1f6555d07317c84536dcc13e4e1cd75c0b3da013a2d918f6"
+    sha256 "fd19cff271298780831c229a34aab48b10eb3849d9e8ae0d0b707dd0beb46112"
   elsif OS.mac? && Hardware::CPU.intel?
     url "https://github.com/christo-auer/eilmeldung/releases/download/0.1.0/eilmeldung-x86_64-apple-darwin-0.1.0.tar.gz"
-
-    sha256 "91da374b4a11da9d813ce9bac52322a4581b83be1f533f1d0726c3ddef7163c7"
+    sha256 "96613a98a4479ce45c1c9d5b15caea7006496ae9896191dd54763de2b8016e4d"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/christo-auer/eilmeldung/releases/download/0.1.0/eilmeldung-x86_64-unknown-linux-gnu-0.1.0.tar.gz"
-
-    sha256 "02eb8b9e345c24c2af6c73a83042982f3397902d81d6f3ca17883a0f292dd82a"
+    sha256 "0b1851cd62807ce9b53f09a9f1f738b55e45f43129a0177ae855339cc8e41e7f"
   end
 
   def install
