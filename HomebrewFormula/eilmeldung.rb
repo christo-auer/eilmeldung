8795cac3a5d83a1523f7e55cd45f9cdb57f58297
Christopher Auer <christopher@auer.team>
2025-12-20 07:40:57 +0000

    chore: update SHA256 in homebrew formula

--- HomebrewFormula/eilmeldung.rb
+++ HomebrewFormula/eilmeldung.rb
@@ -2,7 +2,7 @@ class Eilmeldung < Formula
   desc "a feature-rich TUI RSS reader based on the newsflash library"
   homepage "https://github.com/christo-auer/eilmeldung"
   url "https://github.com/christo-auer/eilmeldung/archive/refs/tags/0.4.0.tar.gz"
-  sha256 "6df4fb18b7f0e24dc908bc5c80a6c7cf028fc9844ed06c01270f136183118912"
+  sha256 "cc4a31054c7ffa5443a26eae17854a9fc973fb460f695d28f035d962294c4893"
   license "GPL-3.0"
   head "https://github.com/christo-auer/eilmeldung.git", branch: "main"
   version "0.4.0"
