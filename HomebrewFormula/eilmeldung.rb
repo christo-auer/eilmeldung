5fc530c1e8b80ac74bcdc7a99457a668556c1d20
Christopher Auer <christopher@auer.team>
2025-12-28 20:53:02 +0100

    chore: Release eilmeldung version 0.4.11

--- HomebrewFormula/eilmeldung.rb
+++ HomebrewFormula/eilmeldung.rb
@@ -1,11 +1,11 @@
 class Eilmeldung < Formula
   desc "a feature-rich TUI RSS reader based on the newsflash library"
   homepage "https://github.com/christo-auer/eilmeldung"
-  url "https://github.com/christo-auer/eilmeldung/archive/refs/tags/0.4.10.tar.gz"
+  url "https://github.com/christo-auer/eilmeldung/archive/refs/tags/0.4.11.tar.gz"
   sha256 "dd7521528e32ff9c7682853e8226af63222d38ead9e2d75ec9b24c82f16bf646"
   license "GPL-3.0"
   head "https://github.com/christo-auer/eilmeldung.git", branch: "main"
-  version "0.4.10"
+  version "0.4.11"
 
   depends_on "pkg-config" => :build
   depends_on "rust" => :build
--- nix/package.nix
+++ nix/package.nix
@@ -13,7 +13,7 @@
 
 rustPlatform.buildRustPackage {
   pname = "eilmeldung";
-  version = "0.4.10";
+  version = "0.4.11";
   
   src = ../.;
   
