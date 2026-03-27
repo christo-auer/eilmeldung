07ef67a941831b12d6cca7adeb13d3dc95971033
Christopher Auer <christopher@auer.team>
2026-03-27 18:40:33 +0100

    minor adjustment in display

--- src/ui/article_content/view.rs
+++ src/ui/article_content/view.rs
@@ -490,11 +490,11 @@ impl ArticleContentViewData {
                 let counter = inner_link_counter.fetch_add(1, Ordering::Relaxed) + 1;
                 url_for_hint.entry(counter).or_insert(url.to_owned());
                 let mut spans = vec![
-                    Span::styled(format!("{url_icon}({counter})"), url_hint_style),
+                    Span::styled(format!("{url_icon}{counter}:"), url_hint_style),
                     Span::styled(alt.to_owned(), alt_url_text_style),
                 ];
                 if show_url {
-                    spans.push(Span::styled(format!(" ({url})"), url_url_text_style));
+                    spans.push(Span::styled(format!("({url})"), url_url_text_style));
                 }
 
                 spans
@@ -504,12 +504,12 @@ impl ArticleContentViewData {
                 let counter = inner_image_counter.fetch_add(1, Ordering::Relaxed) + 1;
                 url_for_hint.entry(counter).or_insert(url.to_owned());
                 let mut spans = vec![
-                    Span::styled(format!("{image_icon}({counter})"), image_hint_style),
+                    Span::styled(format!("{image_icon}{counter}:"), image_hint_style),
                     Span::styled(alt.to_owned(), alt_text_image_style),
                 ];
 
                 if show_url {
-                    spans.push(Span::styled(format!(" ({url})"), url_image_style));
+                    spans.push(Span::styled(format!("({url})"), url_image_style));
                 }
                 spans
             })
